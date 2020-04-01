5dbb04d5cb4026accd2def1a5674397d1ce98f9f
Jiackie <Jiaqili1023@gmail.com>
2020-03-31 23:50:19 -0700

    Updates

---
+++ precache-manifest.86e49e65c1437a0f90621f6c7d9d3d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b78a94b05a724c606dddfe2b76c91e",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "8451007606ce2c041ee3",
+    "url": "/shopping_cart/static/js/2.9a56d00a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopping_cart/static/js/2.9a56d00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844a6b445474bc6aa129",
+    "url": "/shopping_cart/static/js/main.58333896.chunk.js"
+  },
+  {
+    "revision": "6e2336b00352fcf782ea",
+    "url": "/shopping_cart/static/js/runtime-main.52653e7f.js"
+  }
+]);
